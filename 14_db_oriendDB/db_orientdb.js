d90e01f9f34beaa8363f9c7c5ee5ad64682a617c
sg-kim <sgkim08@gmail.com>
2018-11-07 07:07:05 +0900

    web program with DB(orientDB)

--- 14_db_oriendDB/db_orientdb.js
+++ 14_db_oriendDB/db_orientdb.js
@@ -3,9 +3,27 @@
 var orientDB = require('orientjs');
 
 var server = orientDB({
-	host: 'localhost',
+	host: 'localhost',		//	describe location of DB
 	port: 2424,
 	username: 'root',
 	password: '8087'
+});
+
+var db = server.use('alpha');	//	designate DB
+
+//db.record.get('#22:0').then(function(record){
+//	console.log('Loaded record: ', record.title);
+//});
+
+//	Create
+
+var sql = 'SELECT * FROM topic WHERE @rid=:rid';
+var param = {
+	params:{
+		rid:'#22:0'
+	}
+};
+db.query(sql, param).then(function(results){
+	console.log(results);
 });
 
